852e8b5bbe1958afef9b0f3be2349269ec31f1a5
Bohdan <bogdan.dp.16@gmail.com>
2021-12-23 13:25:20 +0200

    reduced the number of requests

--- script.js
+++ script.js
@@ -7,28 +7,14 @@ async function getData() {
 	return await (await fetch('https://swapi.dev/api/people/' + `${pageNum}`)).json();
 }
 
-async function getName() {
-	return await (await getData()).name;
-}
-
-async function getHeight() {
-	return await (await getData()).height;
-}
-
-async function getMass() {
-	return await (await getData()).mass;
-}
-
-async function getGender() {
-	return await (await getData()).gender;
-}
 
 btn.addEventListener('click', async function () {
+	const results = await getData();
 	heroesBlock.innerHTML += `<div class="heroes-item">
-			<p class="heroes-item__text name">${await getName()}</p>
-			<p class="heroes-item__text height">${await getHeight()}</p>
-			<p class="heroes-item__text mass">${await getMass()}</p>
-			<p class="heroes-item__text gender">${await getGender()}</p>
+			<p class="heroes-item__text name">${results.name}</p>
+			<p class="heroes-item__text height">${results.height}</p>
+			<p class="heroes-item__text mass">${results.mass}</p>
+			<p class="heroes-item__text gender">${results.gender}</p>
 			</div>`;
 	pageNum += 1;
 })
